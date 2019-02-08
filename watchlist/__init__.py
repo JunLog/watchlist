143d72baf7714cff8c63c268ba1b4dfc0e162061
JunLog <junlog@foxmail.com>
2019-02-08 18:45:12 +0800

    第十章-项目文件结构

---
+++ watchlist/__init__.py
@@ -0,0 +1,40 @@
+"""
+包构造文件，创建程序实例
+"""
+
+import os
+import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix+os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    from watchlist.models import User
+    user = User.query.get(int(user_id))
+    return user
+
+login_manager.login_view = 'login'
+
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+from watchlist import views, errors, commands
---
+++ watchlist/views.py
@@ -0,0 +1,104 @@
+"""
+视图函数
+"""
+from flask import render_template, request, url_for, redirect, flash
+from flask_login import login_user, login_required, logout_user, current_user
+
+from watchlist import app, db
+from watchlist.models import User, Movie
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = request.form.get('title')
+        year = request.form.get('year')
+
+        if not title or not year or len(year)>4 or len(year)<4 or len(title)>60:
+            flash("Invalid input") # 无效输入
+            return redirect(url_for('index'))
+
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
+    movies = Movie.query.all()
+    return render_template('index.html', movies = movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input!')
+            return redirect(url_for('edit', movie_id=movie_id))
+        
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated!')
+        return redirect(url_for('index'))
+    
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+@login_required
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input!!')
+            return redirect(url_for('login'))
+        user = User.query.first()
+
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login Success!')
+            return redirect(url_for('index'))
+
+        flash("Invalid username or password！！！！")
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required  # 用于视图保护，后面会详细介绍
+def logout():
+    logout_user()  # 登出用户
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # 重定向回首页
+
+@app.route('/settings', methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name)>20:
+            flash('Invalid input')
+            return redirect(url_for('settings'))
+        
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
